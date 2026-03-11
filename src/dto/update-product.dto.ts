de50168f4156cb90406e9965d6d99bc66bbf1d95
Aisha <aisa.youldasheva@gmail.com>
2026-03-12 04:11:29 +0500

    feat(product): add product dtos

---
+++ src/dto/update-product.dto.ts
@@ -0,0 +1,17 @@
+import { IsString, IsNumber } from "class-validator";
+
+export class UpdateProductDto {
+    @IsString()
+    name: string;
+
+    @IsString()
+    description: string;
+    
+    @IsNumber()
+    price: number;
+    
+    @IsString()
+    image: string;
+
+    categoryId: number;
+}
