e2eae6e80bdadce60577d5a525308ddce7442bf6
Aisha <aisa.youldasheva@gmail.com>
2026-03-12 04:27:43 +0500

    feat(export): add excel module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,39 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoriesModule } from './categories/categories.module';
 import { ProductsModule } from './products/products.module';
+import { FilesModule } from './files/files.module';
+import { join } from 'path';
 
 @Module({
   
-  imports: [CategoriesModule, ProductsModule],
+  imports: [
+    ConfigModule.forRoot( { isGlobal: true } ),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService]
+    }),
+
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, 'static'),
+    }),
+
+    CategoriesModule, 
+    ProductsModule,
+    FilesModule
+  ],
 
 })
 export class AppModule {}
---
+++ src/excel/excel.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@nestjs/common';
+import * as ExcelJs from 'exceljs'
+import { Category } from 'src/categories/category.entity';
+import { Product } from 'src/products/product.entity';
+
+@Injectable()
+export class ExcelService {
+    async exportProducts(products: Product[]) {
+        const workbook = new ExcelJs.Workbook()
+        const worksheet = workbook.addWorksheet("Products")
+
+        worksheet.columns = [
+            { header: 'Id', key: 'id', width: 5 },
+            { header: 'Name', key: 'name', width: 25 },
+            { header: 'Description', key: 'description', width: 25 },
+            { header: 'Price', key: 'price' },
+            { header: 'CategoryName', key: 'category' },
+        ];
+
+        products.forEach(product => {
+            worksheet.addRow({
+                id: product.id,
+                name: product.name,
+                description: product.description,
+                price: product.price,
+                category: product.category?.name
+            })
+        })
+
+        return workbook
+    }
+
+    async exportCategories(categories: Category[]) {
+        const workbook = new ExcelJs.Workbook()
+        const worksheet = workbook.addWorksheet("Categories")
+
+        worksheet.columns = [
+            { header: 'Id', key: 'id', width: 10 },
+            { header: 'Name', key: 'name', width: 10 },
+            { header: 'Description', key: 'description', width: 25 }
+        ]
+
+        categories.forEach(category => {
+            worksheet.addRow({
+                id: category.id,
+                name: category.name,
+                description: category.description
+            })
+        })
+
+        return workbook
+    }
+}
