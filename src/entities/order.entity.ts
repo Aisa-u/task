19f8d22808f384eb94e6a5c49b29cf6e2ca81051
Aisha <aisa.youldasheva@gmail.com>
2026-03-12 12:16:13 +0500

    fix(order): fix relations

---
+++ src/entities/order.entity.ts
@@ -0,0 +1,37 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm'
+import { OrderItem } from './order-item.entity'
+
+export enum OrderStatus {
+    PLACED = 'order placed',
+    PROCCES = 'proccessing',
+    DELIVERED = "delivered",
+    CANCEL = 'cancelled'
+}
+
+@Entity()
+export class Order {
+    
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column({ type: 'date' })
+    date: string
+
+    @Column()
+    discount: number
+
+    @Column({ name: 'customerName' })
+    cName: string
+    
+    @Column({
+        type: 'enum',
+        enum: OrderStatus,
+    })
+    status: OrderStatus
+
+    @OneToMany(() => (OrderItem), (item) => item.order, { 
+        cascade: true 
+    })
+    orderItems: OrderItem[]
+
+}
