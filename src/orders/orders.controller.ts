02b4b19eab747129f274f6624e13b9981bf29905
Aisha <aisa.youldasheva@gmail.com>
2026-03-12 14:08:19 +0500

    feat(order): add post decorator to controller

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,7 +1,16 @@
 import { Controller } from '@nestjs/common';
+import { Get, Post, Put, Patch, Delete, Param, Body, Res} from '@nestjs/common';
 import { OrdersService } from './orders.service';
+import { CreateOrderDto } from 'src/dto/create-order.dto';
 
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
+
+  //POST
+  @Post()
+  async createOrder(@Body() dto: CreateOrderDto) {
+    return await this.ordersService.createOrder(dto)
+  }
+
 }
