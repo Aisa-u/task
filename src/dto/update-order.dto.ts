56604b7b36adb42567362206f44f88f968d09389
Aisha <aisa.youldasheva@gmail.com>
2026-03-13 10:25:13 +0500

    feat(order): add order item update dto

--- src/dto/update-order.dto.ts
+++ src/dto/update-order.dto.ts
@@ -1,16 +1,22 @@
-import { IsEnum, IsNumber, IsString } from "class-validator"
+import { IsEnum, IsInt, Min } from "class-validator"
 import { OrderStatus } from "src/entities/order.entity"
 
 export class UpdateOrderDto {
-    date: string
-
-    @IsNumber()
-    discount: number
-
-    @IsString()
-    cName: string
 
     @IsEnum(OrderStatus)
     status: OrderStatus
+
+    orderItems: UpdateOrderItemDto[]
     
+}
+
+export class UpdateOrderItemDto {
+
+    @IsInt()
+    productId: number
+
+    @IsInt()
+    @Min(1)
+    quantity: number
+
 }
